5f1d42292bd52825a9c20fdd66b5f5aa1f02eb68
deebot10 <dee1jhill@gmail.com>
2021-03-01 22:36:09 -0700

    Model A

---
+++ models/model_a.rb
@@ -0,0 +1,14 @@
+# DINOSAUR model
+type = "Megaladon"
+scientific_name = "Otodus megalodon"
+size = 18  # meters
+weight = 66000 # Ibs
+is_extint = true
+is_deadly = true
+prey = ["fish", "baleen whales", "toothed whales", "sperm whales"]
+shark_movies = ["Jaws", "The Meg", "Jaws 2"]
+
+
+gender_length = [male =>  50,  # hash example length in ft
+                female => 60
+               ]
