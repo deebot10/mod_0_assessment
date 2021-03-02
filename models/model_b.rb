f2ad6b1530cb198240d296786f91296ba8af75e6
deebot10 <dee1jhill@gmail.com>
2021-03-01 22:45:48 -0700

    Model B

---
+++ models/model_b.rb
@@ -0,0 +1,14 @@
+# Developer
+company = "Game Freak"
+job_title = "Game Programmer"
+hours_worked_per_week = 48
+years_on_the_job =  5 
+is_working = true
+is_rested = false
+game_classics = ["Pokemon Yellow", "Pokemon Ruby", "Pokemon Red"]
+in_the_works = ["Pokemon Legends Arceus", "Pokemon City"]
+
+ratings = {"Pokemon Yellow" => 10,
+           "Pokemon Ruby" => 8,
+            "Pokemon Red" => 10,
+          }
