ab2ef00e30db29fb80029ac1abad7e1ad7e34560
deebot10 <dee1jhill@gmail.com>
2021-03-01 22:12:03 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+movie = "Judas and the Black Messiah" #Leaves HBO Max soon
+is_entertaining = true
+movie = "Sound of Music"
+
+num_4 = 50
+num_6 = 8
+product = num_4 * num_6
+
+favorite_foods = ["Hawiian chicken katsu", "Pasta", "Sushi"]
+favorite_foods.delete(0)
+
+print movie.length
